2cabb2d99d6049dff58ecc9b4b7bc77e6d65e7eb
Andrei <a.podznoev@reply.de>
2019-01-11 11:49:28 +0100

    Changes to match the gist

--- src/main/java/de/comsystoreply/redislocks/locks/RedisLockWithDeleteCheck.java
+++ src/main/java/de/comsystoreply/redislocks/locks/RedisLockWithDeleteCheck.java
@@ -18,35 +18,26 @@ public class RedisLockWithDeleteCheck {
         if (!locked) {
             return false;
         }
+
         try {
             runnable.run();
             return true;
         } finally {
             redis.execute(
-                    deleteScript(),
+                    new DefaultRedisScript<>(
+                            "local current = redis.call('GET', KEYS[1]) " +
+                                    "if current == ARGV[1] then " +
+                                    "   redis.call(\"del\", KEYS[1]) " +
+                                    "   return ARGV[1] " +
+                                    "else " +
+                                    "   return current " +
+                                    "end ",
+                            String.class
+                    ),
                     Collections.singletonList(lockAttemptId.getKey()),
                     new Object[]{lockAttemptId.getValue()}
             );
         }
-    }
-
-    private static RedisScript<String> deleteScript() {
-//        Redis uses the same Lua interpreter to run all the commands.
-//        Also Redis guarantees that a script is executed in an atomic way:
-//        no other script or Redis command will be executed while a script is being executed.
-//        This semantic is similar to the one of MULTI / EXEC.
-//        From the point of view of all the other clients the effects of a script are either still not visible or already completed.
-//        If a key with expiry (ttl) exists at the start of eval, it will not get expired during the evaluation of a script.
-        return new DefaultRedisScript<>(
-                "local current = redis.call('GET', KEYS[1]) " +
-                        "if current == ARGV[1] then " +
-                        "   redis.call(\"del\", KEYS[1]) " +
-                        "   return ARGV[1] " +
-                        "else " +
-                        "   return current " +
-                        "end ",
-                String.class
-        );
     }
 
     private boolean tryLock(LockAttemptId lockAttemptId) {
